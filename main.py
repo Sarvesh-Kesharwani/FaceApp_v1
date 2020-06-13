c9b226256799b482f97d0c1e0b893f8a0c2473da
sarvesh kesharwani <sarveshkumar5513@gmail.com>
2020-06-13 16:19:37 -0400

    Before experimenting with Making_Button_Functional.

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.navigationdrawer import NavigationLayout,MDNavigationDrawer
+from kivymd.uix.button import Button, MDRaisedButton
+
 
 KV = '''
 <ContentNavigationDrawer>:
-
+    theme_cls:"Yellow"
     ScrollView:
 
         MDList:
@@ -35,11 +37,15 @@ Screen:
             id: screen_manager
 
             Screen:
+                id: register_screen
                 name: "scr 1"
-
-                MDLabel:
-                    text: "Screen 1"
-                    halign: "center"
+                MDRaisedButton:
+                    text:"Photo"
+                    #size_hint: 0.1, 0.3
+                    pos_hint: {"x":0.5, "top":0.5}
+                    background_normal: ''
+                    background_down: ''
+                    on_press: root.Connect()
 
             Screen:
                 name: "scr 2"
@@ -55,6 +61,8 @@ Screen:
                 screen_manager: screen_manager
                 nav_drawer: nav_drawer
                 
+                
+                
                
              
 '''
@@ -64,10 +72,13 @@ class ContentNavigationDrawer(BoxLayout):
     def Connect(self):
         print("pressed")
 
-
+class register_screen():
+    def Connect(self):
+        print("pressed")
 
 class TestNavigationDrawer(MDApp):
     def build(self):
+        self.theme_cls.primary_palette = "Green"  # "Purple", "Red"
         return Builder.load_string(KV)
 
 
