9935b10025d0861edccdcac932f3457c60820d90
sarvesh kesharwani <sarveshkumar5513@gmail.com>
2020-06-13 15:50:29 -0400

    navigation_drawer & toolbar working!

--- main.py
+++ main.py
@@ -1,57 +1,67 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivymd.uix.navigationdrawer import
+from kivymd.uix.navigationdrawer import NavigationLayout,MDNavigationDrawer
 
 KV = '''
+<ContentNavigationDrawer>:
+
+    ScrollView:
+
+        MDList:
+
+            OneLineListItem:
+                text: "Screen 1"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "scr 1"
+
+            OneLineListItem:
+                text: "Screen 2"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "scr 2"
+
 Screen:
+    MDToolbar:
+        id: toolbar
+        pos_hint: {"top": 1}
+        elevation: 10
+        title: "MDNavigationDrawer"
+        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
 
     NavigationLayout:
-
         ScreenManager:
+            id: screen_manager
 
             Screen:
+                name: "scr 1"
 
-                BoxLayout:
-                    orientation: 'vertical'
+                MDLabel:
+                    text: "Screen 1"
+                    halign: "center"
 
-                    MDToolbar:
-                        title: "Navigation Drawer"
-                        elevation: 10
-                        left_action_items: [['menu', lambda x: nav_drawer.toggle_nav_drawer()]]
+            Screen:
+                name: "scr 2"
 
-                    Widget:
-
+                MDLabel:
+                    text: "Screen 2"
+                    halign: "center"
 
         MDNavigationDrawer:
             id: nav_drawer
-            
-            ContentNavigationDrawer:
-                id: cnd
-                
-                Button: 
-                    text: "just testing"
-                    on_press: cnd.call_fufu()
-                    
-                MDList:
 
-                    OneLineListItem:
-                        text: "Screen 1"
-                        on_press:
-                            root.nav_drawer.set_state("close")
-                            root.screen_manager.current = "scr 1"
-        
-                    OneLineListItem:
-                        text: "Screen 2"
-                        on_press:
-                            root.nav_drawer.set_state("close")
-                            root.screen_manager.current = "scr 2"
-            
+            ContentNavigationDrawer:
+                screen_manager: screen_manager
+                nav_drawer: nav_drawer
+                
+               
+             
 '''
 
 
 class ContentNavigationDrawer(BoxLayout):
-    def call_fufu(self):
+    def Connect(self):
         print("pressed")
 
 
